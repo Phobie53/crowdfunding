691dfa4a44c566a6149b7ac44dfcf806719e0ed3
Phobie53 <chevenzo@hotmail.fr>
2016-03-16 09:26:26 +0100

    Suppression DTO et modification Hibernate (create-drop => update)

--- trunk/src/main/java/dto/CategorieDTO.java
+++
@@ -1,9 +0,0 @@
-package dto;
-
-import java.io.Serializable;
-
-public class CategorieDTO implements Serializable {
-
-	private static final long serialVersionUID = 6876955142634710433L;
-
-}
--- trunk/src/main/java/dto/DonDTO.java
+++
@@ -1,9 +0,0 @@
-package dto;
-
-import java.io.Serializable;
-
-public class DonDTO implements Serializable {
-
-	private static final long serialVersionUID = 6876955142634710433L;
-
-}
